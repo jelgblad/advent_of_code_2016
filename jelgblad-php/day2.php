1b0930c107c07c6c17b64d710575f6b5d704fe73
Jonas Elgblad <jonas.elgblad@gmail.com>
2016-12-02 13:46:13 +0100

    day 2 part 2

--- jelgblad-php/day2.php
+++ jelgblad-php/day2.php
@@ -12,44 +12,97 @@ print_info($day);
 // Get input
 $input = read_input($day);
 
-// Initialize
-$min_x = 0;
-$min_y = 0;
-$max_x = 2;
-$max_y = 2;
+// Initialize part 1
 $pos_x = 1;
 $pos_y = 1;
 
+$keys = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
 $input_lines = explode(PHP_EOL, $input);
+
+function move($keys, $instruction){
+
+    global $pos_x, $pos_y;
+
+    $min_x = 0;
+    $min_y = 0;
+    $max_x = count($keys[0]) - 1;
+    $max_y = count($keys) - 1;
+
+    $new_x = $pos_x;
+    $new_y = $pos_y;
+
+    switch($instruction){
+        case "U" : 
+            if($new_y > $min_y) $new_y--;
+            break;
+        case "D" : 
+            if($new_y < $max_y) $new_y++;
+            break;
+        case "L" : 
+            if($new_x > $min_x) $new_x--;
+            break;
+        case "R" : 
+            if($new_x < $max_x) $new_x++;
+            break;
+    }
+
+    if($keys[$new_y][$new_x] === null) return;
+
+    $pos_x = $new_x;
+    $pos_y = $new_y;
+}
 
 $code_a = "";
 
 foreach($input_lines as $line){
-    // echo ">" . $line . PHP_EOL;
 
     $line_instructions = str_split($line);
 
     foreach($line_instructions as $instruction){
 
-        switch($instruction){
-            case "U" : 
-                if($pos_y > $min_y) $pos_y--;
-                break;
-            case "D" : 
-                if($pos_y < $max_y) $pos_y++;
-                break;
-            case "L" : 
-                if($pos_x > $min_x) $pos_x--;
-                break;
-            case "R" : 
-                if($pos_x < $max_x) $pos_x++;
-                break;
-        }
+        move($keys, $instruction);
     }
 
-    $key_index = $pos_y * ($max_x + 1) + $pos_x;
+    // echo $keys[$pos_y][$pos_x] . PHP_EOL;
 
-    $code_a .= $key_index + 1;
+    $code_a .= $keys[$pos_y][$pos_x];
 }
 
-print_solution("a: " . $code_a . ", b: " . "-");
+
+
+
+// Initialize part 2
+$pos_x = 0;
+$pos_y = 2;
+
+$keys = [
+    [null, null, 1, null, null],
+    [null, 2, 3, 4, null],
+    [5, 6, 7, 8, 9],
+    [null, "A", "B", "C", null],
+    [null, null, "D", null, null],
+];
+
+$code_b = "";
+
+foreach($input_lines as $line){
+
+    $line_instructions = str_split($line);
+
+    foreach($line_instructions as $instruction){
+
+        move($keys, $instruction);
+    }
+
+    // echo $keys[$pos_y][$pos_x] . PHP_EOL;
+
+    $code_b .= $keys[$pos_y][$pos_x];
+}
+
+echo "Part 1: " . $code_a . PHP_EOL;
+echo "Part 2: " . $code_b . PHP_EOL;
